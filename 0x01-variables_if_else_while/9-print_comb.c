341daf1e42e4b3556f6c8780686fd4cdb74c1f34
thomcray <ternathompson2@gmail.com>
2022-09-09 14:59:20 -0700

    9-print-comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		putchar('0' + i);
 		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		putchar('0' + i);
 	}
 	putchar('\n');
 	return (0);
