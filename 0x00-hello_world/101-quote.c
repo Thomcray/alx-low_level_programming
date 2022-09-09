2c47362f6f65ba7ad3993ac7dc61319c6c440e3e
thomcray <ternathompson2@gmail.com>
2022-09-08 20:59:52 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return 1 (success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(str);
+
+	return (1);
+}
