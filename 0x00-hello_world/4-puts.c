deeb357f42efece61bf41de1f6d82355cc85ae21
thomcray <ternathompson2@gmail.com>
2022-09-08 18:17:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry point
  *
+ * Description: Main will print out "Programming is like a multilingual puzzle"
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
