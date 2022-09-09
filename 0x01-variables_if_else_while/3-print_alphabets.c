7a6dde6106cc600c84842e0837a72958525a625e
thomcray <ternathompson2@gmail.com>
2022-09-09 09:43:35 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the letters of the alphabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i <= alph[i]; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
