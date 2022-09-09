7b7430c9e1777796dc71edbfcfaf5835b7698709
thomcray <ternathompson2@gmail.com>
2022-09-09 14:54:01 -0700

    9-print-comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit separated by ,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(i);
+	}
+	purchar('\n');
+	return (0);
+}
