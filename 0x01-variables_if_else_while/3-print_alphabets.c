ed89c5246ba9177b96d87322a937138519486ba6
thomcray <ternathompson2@gmail.com>
2022-09-09 09:45:34 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i <= alph[i]; i++)
+	for (i = 0; i <= alpha[i]; i++)
 	{
 		putchar(alpha[i]);
 	}
