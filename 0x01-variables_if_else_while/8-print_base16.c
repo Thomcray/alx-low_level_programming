282fb82394cb7db5836938020beb529b4178a228
thomcray <ternathompson2@gmail.com>
2022-09-09 14:07:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of 16 in lowercase followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char hex[] = "0123456789abcdef";
+	int k;
+
+	for (k = 0; k <= hex[k]; k++)
+	{
+		putchar(hex[k]);
+	}
+	putchar('\n');
+	return (0);
+}
