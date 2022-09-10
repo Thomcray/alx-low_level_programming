9ab48bc653d7a0d4a3af428778298e1ace14c37d
thomcray <ternathompson2@gmail.com>
2022-09-09 21:07:03 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
+	int j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
+			putchar(i + '0');
+			putchar(j + '0');
 
 			if (i == 99 && j == 99)
 			{
