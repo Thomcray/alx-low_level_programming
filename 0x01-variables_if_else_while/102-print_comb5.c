59b22e28bbc0d911561c141f2af8aebbcaea941b
thomcray <ternathompson2@gmail.com>
2022-09-12 14:37:32 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,19 +13,16 @@ int main(void)
 	{
 		for (j = 1; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-
-			if (i == 98 && j == 99)
+			if (i != j && i < j)
 			{
-				continue;
-			}
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
 
-			putchar(',');
-			putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				putchar(',');
+				putchar(' ');
 		}
 	}
 	putchar('\n');
