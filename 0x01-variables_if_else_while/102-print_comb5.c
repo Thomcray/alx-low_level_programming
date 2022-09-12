df7117eb592c4bc15602f518e970ffc01b631dbd
thomcray <ternathompson2@gmail.com>
2022-09-12 14:47:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,8 @@ int main(void)
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
-
+				if (i == 98 && j == 99)
+					continue;
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
 				putchar(',');
