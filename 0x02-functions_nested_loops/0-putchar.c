2304c7fa31522c2e7e71ccb05130d08404314559
thomcray <ternathompson2@gmail.com>
2022-09-13 17:11:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	char putchar_[] = "_putchar";
+
+	for (i = 0; i <= putchar[i]; i++)
+	{
+		_putchar(putchar[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+## Task 0x02-functions_nested_loops
+  1. Print _putchar, followed by a new line. Return 0
