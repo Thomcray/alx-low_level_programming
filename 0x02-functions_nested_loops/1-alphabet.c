23f4759ca217b79b301c4031a048f6635baa59c9
thomcray <ternathompson2@gmail.com>
2022-09-13 20:38:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,17 +14,3 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	print_alphabet();
-
-	_putchar('\n');
-	return (0);
-}
