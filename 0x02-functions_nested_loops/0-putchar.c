333622106653b9b10b86b6a36a6c81f2b52f8cc6
thomcray <ternathompson2@gmail.com>
2022-09-13 19:52:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	putchar_('_');
+	putchar_('p');
+	putchar_('u');
+	putchar_('t');
+	putchar_('c');
+	putchar_('h');
+	putchar_('a');
+	putchar_('r');
 
 	_putchar('\n');
 
