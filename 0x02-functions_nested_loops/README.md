1c4e577539e674dac278a514160b0dd7bce2308d
thomcray <ternathompson2@gmail.com>
2022-09-13 22:31:08 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,4 +2,5 @@
   0. Print _putchar, followed by a new line. Return 0
   1. Print alphabets in lowercase
   2. Print 10 times the alphabet in lowercase followed by newline
-  3. Check for lowercase character 
+  3. Check for lowercase character
+  4. Check for alphabetic character 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,11 @@
  * print_alphabet - Prints alphabets in lowercase
  * print_alphabet_x10 - Prints x10 the alphabets in lowercase
  * _islower - Checks for lowercase character
+ * _isalpha - Checks for alphabetic character
  */
 
 void _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
