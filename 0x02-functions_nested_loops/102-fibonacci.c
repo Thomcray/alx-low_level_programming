7293216ded799675d9ddc45a4bd9f3109261f86c
thomcray <ternathompson2@gmail.com>
2022-09-14 19:26:30 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 fibonacci, starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long first, second, sum;
+
+	for (i = 0; i <= 50; i++)
+	{
+		sum = first + second;
+		printf("%lu", sum);
+
+		first = second;
+		second = sum;
+
+		if (i < 50)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -13,3 +13,4 @@
   11. Prints all natural numbers from n to 98, followed by a new line
   12. Prints n times table, starting with 0
   13. Program that computes the sum of all the multiples of 3 or 5 below 1024(excluded)
+  14. Program that prints the first 50 fibonacci numbers, starting with 1 and 2 followed by newline
