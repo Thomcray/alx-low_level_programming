76910bee819469ac62b67c51f86df12244715f21
thomcray <ternathompson2@gmail.com>
2022-09-14 18:39:36 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) && (i % 5) == 0)
+		if ((i % 3 == 0) || (i % 5) == 0)
 		{
 			sum += i;
 		}
-		printf("%d", sum);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
