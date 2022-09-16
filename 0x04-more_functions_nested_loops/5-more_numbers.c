68cff32ac2674bb3c77aff96220a31ecf59ec91c
thomcray <ternathompson2@gmail.com>
2022-09-16 15:56:24 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			if (j > 9)
+			{
+				putchar((j / 10) + 10);
+			}
+
 			putchar((j % 10) + '0');
 		}
+		putchar('\n');
 	}
-	putchar('\n');
 }
