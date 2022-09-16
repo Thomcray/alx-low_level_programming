d251307cd7dbdb649c1306a354fffee11f2f1d4e
thomcray <ternathompson2@gmail.com>
2022-09-15 23:46:04 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,8 @@
 
 /**
  * print_numbers - prints numbers 0-9, followed by a new line
+ *
+ * Return: Always 0
  */
 
 void print_numbers(void)
@@ -10,5 +12,6 @@ void print_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 		_putchar((num % 10) + '0');
+
 	_putchar('\n');
 }
