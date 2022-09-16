9554d3d40a1ead26b17913fcf90faa3544dccede
thomcray <ternathompson2@gmail.com>
2022-09-15 22:02:04 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,11 +9,8 @@
 
 int _isdigit(int c)
 {
-	for (c = 48; c <= 57; c++)
-	{
-		if (int isdigit(c))
-			return (1);
-		else
-			return (0);
-	}
+	if ((c >= 48) && (c <= 57))
+		return (1);
+	else
+		return (0);
 }
