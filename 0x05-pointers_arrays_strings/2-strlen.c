3f501405f896975a0d2affbfdd82a5916212792c
thomcray <ternathompson2@gmail.com>
2022-09-20 12:46:42 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string parameter
+ *
+ * Return: number of characters in *s
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
