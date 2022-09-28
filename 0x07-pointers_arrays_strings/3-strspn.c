f99e8c875555bf325e82278c1a01328343eedaba
thomcray <ternathompson2@gmail.com>
2022-09-28 13:10:58 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,11 +15,11 @@ unsigned int _strspn(char *s, char *accept)
 
 	value = 0;
 
-	for (i = 0; s[i] = '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
 
-		for (j = 0; accept[j] = '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
