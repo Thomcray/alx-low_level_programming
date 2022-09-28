1a44d2627ff9b4c14d152c361699b1e203bad039
thomcray <ternathompson2@gmail.com>
2022-09-28 13:03:18 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: bytes it consists
+ *
+ * Return: the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, value, check;
+
+	value = 0;
+
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		check = 0;
+
+		for (j = 0; accept[j] = '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				value++;
+				check = 1;
+			}
+		}
+
+		if (check == 0)
+		{
+			return (value);
+		}
+	}
+	return (value);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@
   0. Write a function that fills memory with a constant style.
   1. Write a function that copies memory area
   2. A function that locates a character in a string
+  3. A function that gets the length of a prefix substring
