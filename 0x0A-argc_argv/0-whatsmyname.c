c3fff6a4de80ab6a5c3f1f0a89f8adb48bd43bc5
thomcray <ternathompson2@gmail.com>
2022-10-01 16:27:49 -0700

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -13,9 +13,10 @@ int main(int argc, char *argv[])
 {
 	int i;
 
+	printf("argc = %d\n", argc);
 	printf("Let's see what is in argv[]\n");
 
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("argv[%d] = %s\n", i, argv[i]);
 	}
