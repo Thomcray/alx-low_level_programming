513f158c9db57055582b09d5dd341f1e1b71b144
thomcray <ternathompson2@gmail.com>
2022-10-01 16:13:46 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("Let's see what is in argv[]\n");
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+## TASK 0x0A.C - argc, arg
+  0. write a program that prints its name, followed by a new line
