63216b4d20e0328d31a15675e956af34b62cbb51
thomcray <ternathompson2@gmail.com>
2022-10-01 17:27:29 -0700

    1-args.c README.md

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,4 @@
 ## TASK 0x0A.C - argc, arg
   0. write a program that prints its name, followed by a new line
+  1. write a program that prints the number of arguments passed into it
+    * your program should print a number, followed by a new line.
