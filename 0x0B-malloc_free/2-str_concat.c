e7c4fa85408ccec645ef4fc39a5e372d430033f5
thomcray <ternathompson2@gmail.com>
2022-10-05 07:31:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,11 +31,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
-		concat[i_concat++] = s[1];
+		concat[i_concat++] = s1[i];
 
 	for (i = 0; s2[i]; i++)
 	{
-		concat[i_concat++] = s[2];
+		concat[i_concat++] = s2[i];
 	}
 
 	return (concat);
